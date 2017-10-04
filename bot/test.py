8f611ef8d264edc068d13ff275c6fcb05d04810f
mrmedfac9 <ramefa@hotmail.com>
2017-10-04 11:23:49 +0200

    Test de comprobaciones closes #2

---
+++ bot/test.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import funciones
+import unittest
+
+class Testeador(unittest.TestCase):
+    def Comprobador(self):
+        self.assertEqual(len(funciones.comprobarTrack("PQ48K20440124700118006G")), 4) #Comprobar que la longitud de lo que devuelve la funcion es 4
+        self.assertEqual(funciones.comprobarTrack("234"), -1)
+
+if __name__ == '__main__':
+    unittest.main()
