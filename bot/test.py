6e2ce6a3e40eefc8c11592cc32f9523507832cc8
Medfac9 <ramefa@hotmail.com>
2017-10-17 20:14:33 +0200

    Añadir comprobaciones

--- bot/test.py
+++ bot/test.py
@@ -1,10 +1,13 @@
 import funciones
 import unittest
 
-class Testeador(unittest.TestCase):
-    def Comprobador(self):
-        self.assertEqual(len(funciones.comprobarTrack("PQ48K20440124700118006G")), 4) #Comprobar que la longitud de lo que devuelve la funcion es 4
-        self.assertEqual(funciones.comprobarTrack("234"), -1)
+class RealizaTest(unittest.TestCase):
+    def comprobarFunciones(self):
+        self.assertEqual(len(funciones.cargarWeb("PQ48K20440124700118006G")), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
+        self.assertEqual(len(funciones.nombreEmpresa(documento)), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
+        self.assertEqual(len(funciones.informacionMensaje(documento)), 2) #Comprobar que la longitud de lo que devuelve la funcion es 2
+        self.assertEqual(len(funciones.fechaMensaje(documento)), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
+        self.assertEqual(funciones.comprobarTrack(documento), -1)
 
 if __name__ == '__main__':
     unittest.main()
