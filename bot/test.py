ebc144adc6257a02ce5b8d4e143e24f65a3d0db7
Medfac9 <ramefa@hotmail.com>
2017-10-19 12:11:11 +0200

    Errores leves en test

--- bot/test.py
+++ bot/test.py
@@ -1,13 +1,13 @@
 import funciones
 import unittest
 
-class RealizaTest(unittest.TestCase):
+class Envio(unittest.TestCase):
     def comprobarFunciones(self):
-        self.assertEqual(len(funciones.cargarWeb("PQ48K20440124700118006G")), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
+        self.assertEqual(len(funciones.cargarWeb(n_track)), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
         self.assertEqual(len(funciones.nombreEmpresa(documento)), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
         self.assertEqual(len(funciones.informacionMensaje(documento)), 2) #Comprobar que la longitud de lo que devuelve la funcion es 2
         self.assertEqual(len(funciones.fechaMensaje(documento)), 1) #Comprobar que la longitud de lo que devuelve la funcion es 1
-        self.assertEqual(funciones.comprobarTrack(documento), -1)
+        self.assertEqual(funciones.comprobarTrack(documento), None)
 
 if __name__ == '__main__':
     unittest.main()
