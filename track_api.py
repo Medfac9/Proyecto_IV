6b1625977e470515df95a49c00343948e6638fb3
Medfac9 <ramefa@hotmail.com>
2017-11-19 10:56:53 +0100

    Correccion error leve #11

--- track_api.py
+++ track_api.py
@@ -33,7 +33,7 @@ class statusDocker(Resource):
         }
         return jsonify(schema)
 
-api.add_resource(status, '/status')
+api.add_resource(statusDocker, '/status')
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
