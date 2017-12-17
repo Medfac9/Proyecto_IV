44c7ded1f038a29d56aaf49b8bef29369938df40
Medfac9 <ramefa@hotmail.com>
2017-12-17 12:50:07 +0100

    fabfile closed #15

---
+++ despliegue/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+
+def instalarProyecto():
+	run('sudo git clone https://github.com/Medfac9/Proyecto_IV')
+	run('cd ./Proyecto_IV && sudo pip3 install -r requirements.txt')
+
+def subirApi():
+    run('nohup sudo -E python3 home/ubuntu/Proyecto_IV/flask_api.py', pty=False)
+
+def borrarApi():
+	run('sudo rm -rf ./Proyecto_IV')
+
+def MatarApi():
+    run('sudo pkill python3')
