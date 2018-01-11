466223b6dd89735a348165af5349489a564707f0
Medfac9 <ramefa@hotmail.com>
2018-01-11 19:09:11 +0100

    Error leve

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -5,7 +5,7 @@ def instalarProyecto():
 	run('cd ./Proyecto_IV && sudo pip3 install -r requirements.txt')
 
 def subirApi():
-    run('nohup sudo -E python3 home/ubuntu/Proyecto_IV/flask_api.py', pty=False)
+    run('nohup sudo -E python3 home/ubuntu/Proyecto_IV/track_api.py', pty=False)
 
 def borrarApi():
 	run('sudo rm -rf ./Proyecto_IV')
